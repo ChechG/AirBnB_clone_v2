4469cce8ace884a1df3b845fbddfb9af3e514227
ChechG <2100@holbertonschool.com>
2021-04-15 03:16:52 +0000

    chmod

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Fabric script that distributes archive to web servers """
+""" Fabric script that distributes archive to the web servers """
 from fabric.api import *
 from datetime import datetime
 import os
